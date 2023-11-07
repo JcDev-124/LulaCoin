b1746cbb7301334f584cfc9015702c317af167a3
julio <julio.prog124@gmail.com>
2023-11-07 18:47:47 -0300

    front conectado com back em login

--- front-end/script.js
+++ front-end/script.js
@@ -1,18 +1,15 @@
-console.log('ok');
 function submitForm(event) {
     event.preventDefault(); // Impede o envio do formulário padrão
     
     var username = document.getElementById("floatingInput").value;
     var password = document.getElementById("floatingPassword").value;
     
-    // Construa o objeto de dados para enviar ao servidor
     var data = {
         login: username,
         password: password
     };
 
-    // Faça a solicitação POST para a URL de autenticação
-    fetch('http://localhost:8080/auth/login', {
+    fetch('http://localhost:5000/users', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -20,43 +17,13 @@ function submitForm(event) {
         body: JSON.stringify(data)
     })
     .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro na solicitação.');
+        if (response.status === 200) {
+            window.location.href = 'dashboard.html';
+        } else {
+            console.log('Usuário ou senha incorretos');
         }
-        return response.json();
-    })
-    .then(data => {
-        // Manipule a resposta do servidor, que contém o token
-        var token = data.token;
-        // Você pode agora usar o token para autenticação ou armazená-lo onde for necessário
-        console.log('Token recebido:', token);
-
-        // Agora que você tem o token, você pode usá-lo em outras solicitações
-        fetch('http://localhost:8080/ola', {
-            method: 'GET', // ou 'POST' ou qualquer outro método necessário
-            headers: {
-                'Authorization': 'Bearer ' + token, // Adicione o token ao cabeçalho de autorização
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-if (!response.ok) {
-throw new Error('Erro na solicitação para /ola.');
-}
-return response.text(); // Altere para response.text() para tratar a resposta como uma string
-})
-.then(olaData => {
-// Faça algo com a string recebida de 'http://localhost:8080/ola'
-console.log('Resposta de /ola:', olaData);
-// Você pode manipular a string aqui
-})
-.catch(error => {
-console.error('Ocorreu um erro ao acessar /ola:', error);
-// Lide com erros aqui
-});
     })
     .catch(error => {
         console.error('Ocorreu um erro na autenticação:', error);
-        // Lide com erros de autenticação aqui
     });
 }
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from controller.UserController import user_controller
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/lulacoinsdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
